5d622a5541d10363617ad1fa28418584e90c1892
wheleph <wheleph@gmail.com>
2015-08-23 11:40:12 +0300

    Removed redundant methods

--- src/main/java/io/github/wheleph/grava/model/Board.java
+++ src/main/java/io/github/wheleph/grava/model/Board.java
@@ -3,7 +3,6 @@ package io.github.wheleph.grava.model;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Objects;
 
 public class Board {
     private Map<Player, List<Integer>> playerPits = new HashMap<>();
@@ -19,23 +18,6 @@ public class Board {
     // TODO check that pit index is correct
     public int getPitStoneCount(Player player, int pitIndex) {
         return playerPits.get(player).get(pitIndex);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Board board = (Board) o;
-        return Objects.equals(playerPits, board.playerPits);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(playerPits);
     }
 
     @Override
